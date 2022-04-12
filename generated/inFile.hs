a44abcc2f8f640a55d0bdc4c8107d309fd545295
Jesse Linossier <jlin0076@student.monash.edu>
2022-04-12 12:54:43 +1000

    updated gitignore to ignore generated files

--- generated/inFile.hs
+++ generated/inFile.hs
@@ -1,6 +1,6 @@
-module Task2221 where
+module Task1 where
 
 -- Takes the first n elements from a list
 take' :: Int -> [Int] -> [Int]
 take' n [] = []
-take' n (x:xs) = x : take' (n - 1) xs
+take' n (x:xs) = x ++ take' (n - 1) xs
